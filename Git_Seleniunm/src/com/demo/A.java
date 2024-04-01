65e509788262b3190e05cad9438288d5360cfe0c
Admin <Admin@192.168.1.63>
2024-04-01 17:57:17 +0530

    By Parag Joshi

---
+++ Git_Seleniunm/src/com/demo/A.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class A {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+			System.out.println("Hello parag");
+	}
+	public void m1()
+	{
+		System.out.println("m2 method");
+	}
+
+}
